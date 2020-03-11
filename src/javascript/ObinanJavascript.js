3f40ccb6dcb2d56613fd3f241b09aa24c3374aff
Matt <matthew.morley.ca@gmail.com>
2020-03-11 10:16:06 -0700

    added files from codepen - elise

---
+++ src/javascript/ObinanJavascript.js
@@ -0,0 +1,34 @@
+function frontPiston(/*front piston extended boolean*/) {
+    if (/*boolean var*/) {
+        document.getElementById("FrontPiston").style.backgroundColor = "green";
+    }
+}
+
+function backPiston(/*back piston extended boolean*/) {
+    if (/*boolean var*/) {
+        document.getElementById("BackPiston").style.backgroundColor = "green";
+    }
+}
+
+function arm(/*arm extended boolean*/) {
+    if (/*boolean var*/) {
+        document.getElementById("Arm").style.backgroundColor = "green";
+    }
+}
+
+function buddyClimb(/*buddy climb extended boolean*/) {
+    if (/*boolean var*/) {
+        document.getElementById("BuddyClimb").style.backgroundColor = "green";
+    }
+}
+
+function buddyClimb(/*pawl activation boolean*/) {
+    if (/*boolean var*/) {
+        document.getElementById("Pawl").style.backgroundColor = "green";
+    }
+}
+
+var countDownTime = new Date(/**/).getTime();
+function timer() {
+    document.getElementById("Time").innerHTML = ;
+}
